656acfc8282de03067ba57194124b1a79759baf1
azegas <azegaspa@gmail.com>
2024-10-24 06:44:45 +0300

    login form

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,7 +1,10 @@
+import LoginForm from '../components/auth/LoginForm';
+
 const Login = () => {
     return (
         <div>
             <h1>Login</h1>
+            <LoginForm />
         </div>
     );
 }
