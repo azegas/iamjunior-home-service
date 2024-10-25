42a085f282c7d5bceb559976ddbadbd3a9f343de
azegas <azegaspa@gmail.com>
2024-10-25 06:30:30 +0300

    must be the wrong way to style

--- src/components/category/CategoryList.jsx
+++ src/components/category/CategoryList.jsx
@@ -1,12 +1,14 @@
 import CategoryCard from './CategoryCard';
 import styles from './CategoryList.module.scss';
+import categoryCardStyles from './CategoryCard.module.scss';
 
-const CategoryList = ({ services }) => {    
+const CategoryList = ({ services, className }) => {    
     return (
         <>
-            <div className={styles.categoryList}>
+            {/* Custom class names 'className' can be passed to override the default styling */}
+            <div className={`${styles.categoryList} ${className}`}>
                 {services.map((service) => (
-                    <CategoryCard key={service.id} service={service} />
+                    <CategoryCard key={service.id} service={service} className={categoryCardStyles.categoryCardSidebar}/>
                 ))}
             </div>
         </>
