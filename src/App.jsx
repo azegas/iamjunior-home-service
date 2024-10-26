73bc8754c2eab33b4d7ec33c79de5700c874341c
azegas <azegaspa@gmail.com>
2024-10-26 12:57:18 +0300

    BrowserRouter implementation wtf no idea what I am doing

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,11 @@
 // TODO implement aboslute paths with @ 'npm install path?' instead of ../../
-import Navbar from './components/layout/NavBar';
-import Home from './pages/Home';
-import Services from './pages/Services';
-import Contact from './pages/Contact';
-import About from './pages/About';
-import Login from './pages/Login';
-import SearchResults from './pages/SearchResults';
-import Page404 from './pages/404';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { RouterProvider } from "react-router-dom";
+import router from './routing/router';
 import './styles/reset.css'; // must be imported first, before ot
 import './styles/global.scss';
 
-function App() {
-
-  // TODO fix routes as in the example
-  // TODO add search page in navbar su active ir pan
-  return (
-    <BrowserRouter>
-      <div className="app">
-        <Navbar />
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/search/:serviceName" element={<SearchResults/>} />
-            <Route path="*" element={<Page404 />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
-}
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
---
+++ src/routing/router.jsx
@@ -0,0 +1,39 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from '../pages/Home';
+import Services from '../pages/Services';
+import Contact from '../pages/Contact';
+import About from '../pages/About';
+import Login from '../pages/Login';
+import SearchResults from '../pages/SearchResults';
+import Page404 from '../pages/404';
+import Navbar from '../components/layout/NavBar';
+import { Outlet } from "react-router-dom";
+
+// Define the Root component
+const Root = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+};
+
+// Define the routes
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { path: "", element: <Home /> },
+      { path: "services", element: <Services /> },
+      { path: "contact", element: <Contact /> },
+      { path: "about", element: <About /> },
+      { path: "login", element: <Login /> },
+      { path: "search/:serviceName", element: <SearchResults /> },
+      { path: "*", element: <Page404 /> },
+    ],
+  },
+]);
+
+export default router;
