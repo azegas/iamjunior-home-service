492e0ac448a117ed1445e161ee9f2366eca7da20
azegas <azegaspa@gmail.com>
2024-10-27 11:32:24 +0200

    some comments

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,21 @@ import Hero from '../components/common/Hero';
 import Loading from '../components/common/Loading';
 import BusinessList from '../components/business/BusinessList';
 import useFetch from '../hooks/use-fetch';
+import Spinner from 'react-bootstrap/Spinner';
 
 const Home = () => {
-    const { data: services } = useFetch('http://localhost:8000/services');
-    const { data: businesses, loading: businessesLoading } = useFetch('http://localhost:8000/businesses');
+    const servicesFetch = useFetch('http://localhost:8000/services');
+    const services = servicesFetch.data;
+    
+    const businessesFetch = useFetch('http://localhost:8000/businesses');
+    const businesses = businessesFetch.data;
+    const businessesLoading = businessesFetch.loading;
+    // console.log('businessesLoading', businessesLoading); //since it is true at first, loading component is shown
 
     return (
         <>
             {services && <Hero services={services} />}
-            {/* Show loading component while businesses are being fetched */}
+            {/* Since initial businessesLoading is true - show loading component while businesses are being fetched */}
             {businessesLoading && <Loading />}
             {/* 
             && - If left is false (null), then it does not output what is on the right (BusinessList component). 
--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -1,12 +1,15 @@
 import SearchResultsCetegoryList from '../components/search/SearchResultsCetegoryList';
 import useFetch from '../hooks/use-fetch';
 import '../styles/global.scss';
+import Loading from '../components/common/Loading';
 
 const SearchResults = () => {
     const { data: services } = useFetch('http://localhost:8000/services');
     const { data: businesses } = useFetch('http://localhost:8000/businesses');
 
-    if (!services || !businesses) return <div className="loading">Loading...</div>;
+    // do loading for the WHOLE page (both category list and businisses list), doing this on purpose to show loading component for the whole page
+    // also, category component needs to know about businisses, so both have to be loaded
+    if (!services || !businesses) return <Loading />;
 
     return (
         <>
