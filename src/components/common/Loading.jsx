444a5cad8ea35f7f764d9cabe4294b3286765f1d
azegas <azegaspa@gmail.com>
2024-10-28 05:07:10 +0200

    rename class

--- src/components/common/Loading.jsx
+++ src/components/common/Loading.jsx
@@ -2,7 +2,7 @@ import '../../styles/global.scss';
 import Spinner from './Spinner';
 
 const Loading = () => {
-    return <div className="loading"><Spinner /></div>;
+    return <div className="center"><Spinner /></div>;
 };
 
 export default Loading;
