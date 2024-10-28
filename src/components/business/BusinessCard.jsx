a1f367a2cf64238012c7118fc34956eec67387c3
azegas <azegaspa@gmail.com>
2024-10-28 08:48:52 +0200

    nicer business cards

--- src/components/business/BusinessCard.jsx
+++ src/components/business/BusinessCard.jsx
@@ -3,11 +3,11 @@ import styles from './BusinessCard.module.scss';
 const BusinessCard = ({ business }) => {
     return (
         <div className={styles.businessCard}>
-            <img src={business.image} alt={business.name} />
-            <p>{business.category}</p>
-            <p>{business.name}</p>
-            <p>{business.address}</p>
-            <p>{business.worker}</p>
+            <img className={styles.image} src={business.image} alt={business.name} />
+            <p className={styles.category}>{business.category}</p>
+            <p className={styles.name}>{business.name}</p>
+            <p className={styles.worker}>{business.worker}</p>
+            <p className={styles.address}>{business.address}</p>
             <button>Book Now</button>
         </div>
     );
