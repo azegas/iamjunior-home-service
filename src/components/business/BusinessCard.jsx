06dba72607c688048fe83e988497b38efcd42faf
azegas <azegaspa@gmail.com>
2024-10-29 19:06:07 +0200

    favorites attempt - able to add/remove to localstorage

--- src/components/business/BusinessCard.jsx
+++ src/components/business/BusinessCard.jsx
@@ -1,14 +1,20 @@
 import styles from './BusinessCard.module.scss';
+import { Link } from 'react-router-dom';
 
-const BusinessCard = ({ business }) => {
+const BusinessCard = ({ business, onFavoriteClick }) => {
     return (
         <div className={styles.businessCard}>
-            <img className={styles.image} src={business.image} alt={business.name} />
-            <p className={styles.category}>{business.category}</p>
-            <p className={styles.name}>{business.name}</p>
-            <p className={styles.worker}>{business.worker}</p>
-            <p className={styles.address}>{business.address}</p>
-            <button>Book Now</button>
+            <img className={styles.favoriteButton} src="https://img.icons8.com/?size=100&id=DFU1kReSUccu&format=png&color=000000" alt="Favorite" onClick={onFavoriteClick} />
+            <Link to={`/business/${business.id}`} className={styles.businessLink}>
+                <img className={styles.image} src={business.image} alt={business.name} />
+                <div className={styles.info}>
+                    <p className={styles.category}>{business.category}</p>
+                    <p className={styles.name}>{business.name}</p>
+                    <p className={styles.worker}>{business.worker}</p>
+                    <p className={styles.address}>{business.address}</p>
+                </div>
+                <button className={styles.bookButton}>Book Now</button>
+            </Link>
         </div>
     );
 };
