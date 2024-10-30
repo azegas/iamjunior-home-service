4d0fd730f268dde7fa82d6dd7545b62e08852bb8
azegas <azegaspa@gmail.com>
2024-10-30 10:55:45 +0200

    change favorite image depending if the business is in the favorites list or not

--- src/components/business/BusinessCard.jsx
+++ src/components/business/BusinessCard.jsx
@@ -1,14 +1,20 @@
 import styles from './BusinessCard.module.scss';
 import { Link } from 'react-router-dom';
 import { useUser } from '../../context/UserContext';
+import { useFavorite } from '../../context/FavoriteContext';
 
 
 const BusinessCard = ({ business, onFavoriteClick }) => {
     const { user } = useUser();
+    const { favorites } = useFavorite();
 
     return (
         <div className={styles.businessCard}>
-            {user && <img className={styles.favoriteButton} src="https://img.icons8.com/?size=100&id=DFU1kReSUccu&format=png&color=000000" alt="Favorite" onClick={onFavoriteClick} />}
+            
+            {user && <img className={styles.favoriteButton} 
+            src={favorites.includes(business.id) ? "https://img.icons8.com/?size=100&id=DFU1kReSUccu&format=png&color=000000" : "https://img.icons8.com/?size=100&id=12306&format=png&color=000000"} 
+            alt="Favorite" onClick={onFavoriteClick} />}
+
             <Link to={`/business/${business.id}`} className={styles.businessLink}>
                 <img className={styles.image} src={business.image} alt={business.name} />
                 <div className={styles.info}>
