ca13281d6fe78a0f15d70e82a22bb8e8d1e60786
azegas <azegaspa@gmail.com>
2024-10-30 07:11:26 +0200

    remove favorite logic from contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,9 @@
 import '../styles/global.scss';
 import { useUser } from '../context/UserContext';
-import { useFavorite } from '../context/FavoriteContext';
 
 const Contact = () => {
     const { user } = useUser();
-    const { favorites } = useFavorite();
 
-    console.log('favorites', favorites );
     return (
         <div className="container">
             <h1 className="title">Contact Us, {user ? user.username : ''}</h1>
