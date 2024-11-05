3ff33be734c4e2a828f78878cb5a3c65d8d6b348
azegas <azegaspa@gmail.com>
2024-11-05 17:57:38 +0200

    api-docs now works, swagger implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,32 @@ const morgan = require("morgan");
 require("dotenv").config();
 const app = express();
 const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+
+// ----------------------------------------------------------------
+// SWAGGER CONFIGURATION
+// ----------------------------------------------------------------
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API Documentation",
+      version: "1.0.0",
+      description: "API for Home Service Application",
+    },
+    servers: [
+      {
+        url: `http://${process.env.API_HOST}:${process.env.API_PORT}`,
+      },
+    ],
+  },
+  apis: ["./src/**/*.js"], // where swagger should look for api endpoints (basically in all files)
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // ----------------------------------------------------------------
 // MIDDLEWARES 
@@ -38,6 +64,7 @@ const { bookingsRouter } = require('./bookings');
 app.use('/api/categories', categoriesRouter);
 app.use('/api/businesses', businessesRouter);
 app.use('/api/bookings', bookingsRouter);
+
 // ----------------------------------------------------------------
 // SERVER
 // ----------------------------------------------------------------
