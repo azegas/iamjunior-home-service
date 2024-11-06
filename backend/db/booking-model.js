86c7d757eaee48e9cc19fab8b6461563dc448408
azegas <azegaspa@gmail.com>
2024-11-06 09:01:18 +0200

    create booking model

---
+++ backend/db/booking-model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema({
+    businessId: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Business', 
+        required: true 
+    },
+    date: { 
+        type: String, 
+        required: true 
+    },
+    time: { 
+        type: String, 
+        required: true 
+    },
+    userEmail: { 
+        type: String, 
+        required: true 
+    },
+    userName: { 
+        type: String, 
+        required: true 
+    },
+    status: { 
+        type: String, 
+        required: true 
+    },
+});
+
+const BookingModel = mongoose.model('Booking', bookingSchema);
+
+module.exports = {
+    BookingModel
+};
