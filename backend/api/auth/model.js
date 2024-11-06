1dfeef202c256a31ea8335594e9f4125bfeea99b
azegas <azegaspa@gmail.com>
2024-11-06 21:18:26 +0200

    has user password before saving to db
    when checking if password is correct - we take the password that the user inputted - hash it - compare to the one in the db, doing some magic, then saying if its correct or not

--- backend/api/auth/model.js
+++ backend/api/auth/model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     username: { 
@@ -20,9 +21,18 @@ const userSchema = new mongoose.Schema({
     versionKey: false
 });
 
-// custom method to check if password is correct
+
+// hash password before saving to database
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+      this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+});
+
+// custom method to check if hashed password is correct
 userSchema.methods.isCorrectPassword = function (password) {
-    return password === this.password;
+    return bcrypt.compare(password, this.password);
 };
 
 const UserModel = mongoose.model('User', userSchema);
