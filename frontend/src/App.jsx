49995d6b6cf7b683a2f9ead71ef5ea893b4e52e3
azegas <azegaspa@gmail.com>
2024-11-09 18:01:01 +0200

    adjust toast messages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,18 @@ const App = () => {
     <UserProvider>
       <FavoriteProvider>
         <RouterProvider router={router} />
-        <ToastContainer />
+        <ToastContainer
+          position="top-center"
+          autoClose={2500}
+          hideProgressBar={false}
+          newestOnTop={false}
+          // closeOnClick
+          rtl={false}
+          // pauseOnFocusLoss
+          // draggable
+          // pauseOnHover
+          theme="light"
+        />
       </FavoriteProvider>
     </UserProvider>
   );
