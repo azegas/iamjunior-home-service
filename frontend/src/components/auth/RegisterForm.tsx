583ea8df69d81624c0363ea48ea12804925ceea7
azegas <azegaspa@gmail.com>
2024-11-27 07:49:34 +0200

    more strict registration requirements

--- frontend/src/components/auth/RegisterForm.tsx
+++ frontend/src/components/auth/RegisterForm.tsx
@@ -18,7 +18,13 @@ const RegisterForm = () => {
       .min(3, 'Username must be at least 3 characters long.')
       .required('Username is required'),
     email: yup.string().email('Invalid email address').required('Email is required'),
-    password: yup.string().required('Password is required'),
+    password: yup
+      .string()
+      .min(8, 'Password must be at least 8 characters long.')
+      .max(32, 'Password must be at most 32 characters long.')
+      .matches(/[A-Z]/, 'Password must contain at least one uppercase letter.')
+      .matches(/[0-9]/, 'Password must contain at least one number.')
+      .required('Password is required'),
   });
 
   const handleRegistrationResponse = (response: Response, data: LoginResponse) => {
