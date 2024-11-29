ec6c8d6d10cd456c82cbd04f5249344d764290e5
azegas <azegaspa@gmail.com>
2024-11-29 07:19:12 +0200

    implement coverage

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -10,4 +10,9 @@ export default {
   },
 
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  collectCoverage: true, // Enable coverage collection
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}', // Specify which files to collect coverage from
+    '!src/**/*.test.{js,jsx,ts,tsx}', // Exclude test files
+  ],
 };
