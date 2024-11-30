53b7f61cb67a7fef1a31bdb4cdb17709d46d3e22
azegas <azegaspa@gmail.com>
2024-11-30 09:31:55 +0200

    random refactor

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-export const app = express();
 import { swaggerMiddleware } from './middlewares/swaggerMiddleware';
 import { globalMiddlewares } from './middlewares/globalMiddlewares';
 import { startServer } from './start-server';
 import { setupRouter } from './setup-router';
+
+const app = express();
 
 globalMiddlewares(app);
 swaggerMiddleware(app);
