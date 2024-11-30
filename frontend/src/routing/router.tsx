b49b7a3c6e741e45098fc4bf06a419a5f3c322bb
azegas <azegaspa@gmail.com>
2024-11-30 13:10:59 +0200

    move navbar modules to common folder

--- frontend/src/routing/router.tsx
+++ frontend/src/routing/router.tsx
@@ -9,7 +9,7 @@ import Login from '../pages/Login';
 import Register from '../pages/Register';
 import Search from '../pages/Categories';
 import Page404 from '../pages/404';
-import Navbar from '../components/layout/NavBar';
+import Navbar from '../components/common/NavBar';
 import Dashboard from '../pages/Dashboard';
 import BusinessDetail from '../components/business/BusinessDetail';
 import Favorites from '../pages/Favorites';
